4b3fe1941680371836bf2c26c47e0365bd17d40d
Taila Rodrigues <taiciric@gmail.com>
2022-11-15 18:31:11 -0300

    Finalizei a API

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import path from 'node:path';
+
+import { router } from './router';
 
 
 mongoose.connect('mongodb://localhost:27017')
   .then(() => {
     const app = express();
-    const port = 3001;
+    const port = 3002;
+
+    app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+    app.use(express.json());
+    app.use(router);
 
     app.listen(port, () => {
       console.log(`🚀 Server is running on http://localhost:${port}`);
